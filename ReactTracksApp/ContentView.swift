c52184b8ca648ac76e265d8e2ec9f657a48b7940
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-11-21 18:41:38 +0200

    Initial Commit

---
+++ ReactTracksApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ReactTracksApp
+//
+//  Created by Ramill Ibragimov on 21.11.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReactTracksApp/ReactTracksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReactTracksAppApp.swift
+//  ReactTracksApp
+//
+//  Created by Ramill Ibragimov on 21.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct ReactTracksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
